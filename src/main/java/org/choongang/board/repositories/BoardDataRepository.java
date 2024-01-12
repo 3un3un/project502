9789d107acdaad81dd67c0397d0af74e30ef2915
3un3un <abc1987042@naver.com>
2024-01-12 14:59:44 +0900

    삭제하기

--- src/main/java/org/choongang/board/repositories/BoardDataRepository.java
+++
@@ -1,8 +0,0 @@
-package org.choongang.board.repositories;
-
-import org.choongang.board.entities.BoardData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardDataRepository extends JpaRepository<BoardData, Long> {
-
-}
